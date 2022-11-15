52e5f5b6c5b2b401fd5f265e2ff42906578e28ac
Qudsiya <qudsiyashafiq@telusinternational.com>
2022-11-15 12:20:40 +0530

    push

--- timesheet/src/Pages/Errors.js
+++ timesheet/src/Pages/Errors.js
@@ -3,7 +3,7 @@ import React from "react";
 const ErrorModal = (props) => {
     return(
         <div>
-            Error Message:{props.error_message}<br />
+            Error Message: {props.error_message}<br />
             Error Status :{props.error_status}<br />
             Error Segment : {props.error_segment}<hr />
         </div>
