9db89e353ec76e96d6a907ba43719b9e70b7d8ae
peeyushyogitelus <peeyush.yogi@telusinternational.com>
2022-11-15 12:18:06 +0530

    Error Page

--- timesheet/src/Pages/Errors.js
+++ timesheet/src/Pages/Errors.js
@@ -3,9 +3,9 @@ import React from "react";
 const ErrorModal = (props) => {
     return(
         <div>
-            Error Message: {props.error_message}<br />
-            Error Status :{props.error_status}<br />
-            Error Segment : {props.error_segment}<hr />
+            Error Message:= {props.error_message}<br />
+            Error Status := {props.error_status}<br />
+            Error Segment := {props.error_segment}<hr />
         </div>
     )  
 }
